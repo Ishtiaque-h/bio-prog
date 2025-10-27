8f884c8437923d49bcc94113f255b339a01d1fe3
Zainab Alhejairi <zhejairi@udel.edu>
2025-10-26 22:28:59 -0400

    add: format check function

--- bio/io.py
+++ bio/io.py
@@ -4,5 +4,14 @@
  I/O files (FASTA/ FASTQ readers & writers)
 ------------------------------------------------
 '''
+def detect_format(filename):
+    # Check file extension to know if it's FASTA or FASTQ
+    if filename.endswith((".fasta", ".fa", ".fna", ".txt")):
+        return "FASTA"
+    elif filename.endswith(".fastq"):
+        return "FASTQ"
+    else:
+        return None
+
 
 
