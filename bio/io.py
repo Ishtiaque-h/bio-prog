9202fb922f503e8542d875864f2c7072c1462155
Zainab Alhejairi <zhejairi@udel.edu>
2025-10-28 10:53:25 -0400

    open file function is added as requested.

--- bio/io.py
+++ bio/io.py
@@ -4,28 +4,14 @@
  I/O files (FASTA/ FASTQ readers & writers)
 ------------------------------------------------
 '''
-import os
+def open_file(filename):
+    """Open a sequence file."""
+    if not os.path.exists(filename):
+        print('Error - Invalid filename.')
+        return None
 
-filename = input('Enter the path to the sequence file: ')
-if not os.path.exists(filename):
-  print('Error - Invalid filename.')
-
-if filename.lower().endswith(('fasta','fa','fna','txt')):
-  sequences = []
-  with open(filename,'r') as f:
-    header = None
-    sequence = ''
-    for line in f:
-      line =line.strip()
-      if line.startswith('>'):
-        header = line[1:].split()[0]
-        seq = []
-      else:
-        seq.append(line)
-elif filename.lower().endswith(('fastq','fq')):
-  pass
-else:
-  print('Error - Wrong File Format. Only fasta and fastq formats are allowed.')
+    
+    return open(filename, 'r') as f
 
 
 
