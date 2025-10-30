e523d7ff956086e5c7b93d56a3014fe5ea746276
Ishtiaque-h <ishti.jewel@gmail.com>
2025-10-29 21:43:13 -0400

    feat: in ops, extract function is added

--- tasks/ops.py
+++ tasks/ops.py
@@ -5,4 +5,20 @@
 ------------------------------------------------
 '''
 
+#---------------imports------------------------------
+import random
+from pathlib import Path
+from typing import List, Tuple
+from bio import io
+
+#---------------Extracting------------------------------
+def extract_random(input_path: Path, fmt: str, out_path: Path, k: int = 25) -> None:
+    if fmt == "fasta":
+        seqs = list(io.read_fasta(input_path))
+        chosen = random.sample(seqs, k=min(k, len(seqs)))
+        io.write_fasta(chosen, out_path)
+    else:
+        seqs = list(io.read_fastq(input_path))
+        chosen = random.sample(seqs, k=min(k, len(seqs)))
+        io.write_fastq(chosen, out_path)
 
